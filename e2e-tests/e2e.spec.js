9db21e320645b92a1b973e33705cb9940e535b5d
Marko-S-O <rek@orasaari.com>
2024-09-21 14:24:19 +0300

    Update e2e.spec.js

--- e2e-tests/e2e.spec.js
+++ e2e-tests/e2e.spec.js
@@ -7,3 +7,13 @@ describe('Pokedex', () => {
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 })
+
+describe('Navigation', () => {
+  test('Can navigate from frontpage to Ivysaur', async ({ page }) => {
+    await page.goto('http://127.0.0.1:8080/')
+    const link = await page.locator('a[href="/pokemon/ivysaur"]')
+    await link.click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+})
+

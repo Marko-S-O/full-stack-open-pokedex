80bf77f3bc3818786adbe46a1419bcaafd114f49
Marko-S-O <rek@orasaari.com>
2024-09-28 12:10:06 +0300

    prod depl to fly.io

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, useMatch } from 'react-router-dom'
+import { Routes, Route, useMatch, useLocation } from 'react-router-dom'
 import { useApi } from './useApi'
 import LoadingSpinner from './LoadingSpinner'
 import ErrorMessage from './ErrorMessage'
@@ -13,6 +13,15 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
 })))
 
 const App = () => {
+
+  // Adding app.get in app.js did not work, at least in the development environment.
+  // React Router steals the /version request and I was not able to find a way to prevent that.
+  // To mitigate, add returning the version number also to the front-end
+  const location = useLocation()
+  if (location.pathname === '/version') {
+    return ('1')
+  }
+
   const match = useMatch('/pokemon/:name')
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
 
